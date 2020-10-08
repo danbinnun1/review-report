168e9a3bf09a198125d2ab9e1e076344b4eaa252
dan binnun <danbinnun1@gmail.com>
2020-10-09 00:27:06 +0300

    fixed bug in bad percentage calculation

--- src/grade/cache-grading/webpage-grading/WebPageGrader.cpp
+++ src/grade/cache-grading/webpage-grading/WebPageGrader.cpp
@@ -4,22 +4,30 @@
 
 grading::WebPageGrader::WebPageGrader(URLOpener *opener, LinksCache *cache) : LinksCacheGrader(cache), m_opener(opener) {}
 
-double grading::WebPageGrader::gradeInterestingLinks(const std::vector<std::string> &links) const {
+double grading::WebPageGrader::gradeInterestingLinks(const std::vector<std::string> &links) const
+{
+    if (links.size() == 0)
+        return 0;
     uint32_t badReviews = 0;
-    uint16_t notBadReviews = 0;
-    for (const std::string &link : links) {
+    uint32_t notBadReviews = 0;
+    for (const std::string &link : links)
+    {
         m_opener->openLink(link);
         ReviewType reviewType = askUser();
-        if (reviewType == ReviewType::BAD) {
+        if (reviewType == ReviewType::BAD)
+        {
             ++badReviews;
         }
-        else if (reviewType == ReviewType::NOT_BAD) {
+        else if (reviewType == ReviewType::NOT_BAD)
+        {
             ++notBadReviews;
         }
-        else if (reviewType == ReviewType::UNINTERESTING) {
+        else if (reviewType == ReviewType::UNINTERESTING)
+        {
             m_cache->insertLink(link);
             ++notBadReviews;
         }
     }
-    return badReviews / (badReviews + notBadReviews);
+    const double badPercentage = ((double)badReviews) / (badReviews + notBadReviews);
+    return badPercentage;
 }
